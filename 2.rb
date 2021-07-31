5dc29f7b40fae35fa995cebf646bc277d1877036
chikara-dot <victorycastlepower0209@gmail.com>
2021-07-31 02:21:07 +0000

    7/31

---
+++ 2.rb
@@ -0,0 +1,18 @@
+hash = {"Leomn" => 100, "Orange" => 150, "Banana" => 250 }
+
+print("eachメソッド","\n");
+hash.each{|key, value|
+ print(key + "=>", value,"\n")
+}
+
+
+print("each_keyメソッド","\n");
+hash.each_key{|key|
+ print("key = " + key,"\n")
+}
+
+
+print("each_valueメソッド","\n");
+hash.each_value{|value|
+ print("value = ", value,"\n")
+}
